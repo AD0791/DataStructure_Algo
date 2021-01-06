0648e9fc6d63c44549bde93dab1911b78d6eb9f5
AD0791 <alexdisla1991@gmail.com>
2021-01-06 15:20:44 -0500

    linked_list | nous komanse construit pas nu an
    Nous kreye klass pou node la et klass linked_list pas nous a. nou komanse
    mete methode pou ajouter yon node nan fin an.

--- linked_list.dart
+++ linked_list.dart
@@ -1,1 +1,49 @@
 // Linked List
+
+ // import 'dart:_internal'; -> LinkedList class
+
+
+
+// linked list la dwe ka:
+// addfirst
+// addlast
+// deletefirst
+// deletelast
+// contains
+// indexOf
+
+
+void main() {
+  var exemple = Llist();
+  exemple.addLast(10);
+  exemple.addLast(123);
+}
+
+class _NodeClass{
+  // node la bezwen yon valeur kanmenm!
+  int value;
+  _NodeClass(this.value);
+  // konnen node ki deyel la
+  _NodeClass next;
+}
+
+class Llist{
+  // linklist la dwe konnen premier et dernier node la
+  _NodeClass _first;
+  _NodeClass _last;
+
+  // function sa ap fenn mete yon linklist en dernier
+  void addLast(int data){
+    // nous creer yon node
+    _NodeClass nodeObject = _NodeClass(data);
+
+    if(_first == null){
+      _first = _last = nodeObject;
+    } else{
+      _last.next = nodeObject;
+      _last = nodeObject;
+    }
+  }
+
+
+}
