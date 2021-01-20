d68a86ff2acd62fbca721513fd0f010e65c453e6
AD0791 <alexdisla1991@gmail.com>
2021-01-20 11:36:10 -0500

    remove some issues in the code

--- linked_list.dart
+++ linked_list.dart
@@ -1,9 +1,5 @@
 // Linked List
-
  // import 'dart:_internal'; -> LinkedList class
-
-
-
 // linked list la dwe ka:
 // addfirst
 // addlast
@@ -11,7 +7,7 @@
 // deletelast
 // contains
 // indexOf
-import 'package:kt_dart/exception.dart';
+//import 'package:kt_dart/exception.dart';
 
 
 void main() {
@@ -22,7 +18,10 @@ void main() {
   exemple.addLast(23);
   exemple.addFirst(3);
   exemple.addFirst(29);
+  exemple.addLast(100);
+  exemple.addLast(1000);
   exemple.deleteFirst();
+  exemple.deleteLast();
 
   print(exemple.indexOf(123));
   print(exemple.contains(3));
@@ -33,17 +32,19 @@ class _NodeClass{
   int value;
   _NodeClass(this.value);
   // konnen node ki deyel la (link la)
-  _NodeClass next;
+  dynamic next;
 }
 
 class Llist{
   // linklist la dwe konnen premier et dernier node la
-  _NodeClass _first;
-  _NodeClass _last;
+  dynamic _first;
+  dynamic _last;
 
   bool _isEmpty(){
     return _first == null;
   }
+
+  
 
   // function sa ap fenn mete yon linklist en dernier
   void addLast(int item){
@@ -57,6 +58,7 @@ class Llist{
       _last = nodeObject;
     }
   }
+
 
   void addFirst(int item){
     _NodeClass node = _NodeClass(item);
@@ -86,7 +88,7 @@ class Llist{
 
   void deleteFirst(){
     if(_isEmpty()){
-      throw new NoSuchElementException();
+      throw new Exception("No Such Element");
     }
 
     if(_first == _last){
@@ -97,6 +99,21 @@ class Llist{
     var second = _first.next;
     _first.next = null;
     _first = second;
+  }
+
+  void deleteLast(){
+   var previous = _getPrevious(_last);
+   _last = previous;
+   _last.next = null;
+  }
+
+  dynamic _getPrevious(_NodeClass noeud){
+    dynamic current = _first;
+    while(current!=null){
+      if (current.next == noeud) return current;
+      current = current.next;
+    }
+    return null;
   }
 
 
